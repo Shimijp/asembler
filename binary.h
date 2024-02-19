e4630c43fe23b4f2bccfc649ca8acf1ba3cd95cd
shimijp <shimiya18@gmail.com>
2024-02-20 01:41:48 +0200

    third commit

---
+++ binary.h
@@ -0,0 +1,20 @@
+//
+// Created by shimon2 on 19/02/2024.
+//
+
+#ifndef ASEMBLER2_BINARY_H
+#define ASEMBLER2_BINARY_H
+#define BASE 2
+#define ZERO_CHAR '0'
+#define ONE_CHAR '1'
+#include "globals.h"
+
+char * dec_to_bin(int num , int bit_size);
+bool  two_com(char * bin);
+int bin_to_dec(char *bin);
+bool  add_one(char * bin);
+void revrse(char * bin);
+
+
+
+#endif //ASEMBLER2_BINARY_H
