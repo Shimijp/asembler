853ad7c83d0bda8b66d4ac051727dfeaf1205f08
shimon daniel <shimiya18@gmail.com>
2024-02-20 12:36:16 +0200

    fourth commit

---
+++ memory.h
@@ -0,0 +1,19 @@
+//
+// Created by shimon2 on 20/02/2024.
+//
+
+#ifndef ASEMBLER2_MEMORY_H
+#define ASEMBLER2_MEMORY_H
+#include <stdlib.h>
+#include <stdio.h>
+#include "globals.h"
+
+bool check_malloc(void *ptr);
+bool check_file(FILE *fp);
+
+
+
+
+
+
+#endif //ASEMBLER2_MEMORY_H
