d05ded19a96e1dbd8707e4558c8d86c4cd345132
shimon daniel <shimiya18@gmail.com>
2024-02-22 12:02:09 +0200

    sixth commit

--- main.c
+++ main.c
@@ -2,18 +2,13 @@
 // Created by shimon2 on 19/02/2024.
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "binary.h"
 
 int main(int argc , char ** argv)
 {
 
-    int num1;
-    char * res=dec_to_bin(  -2, 14);
-    printf("in binary: %s\n",res);
-    printf("numeric value: %lld\n", atoll(res));
-    printf("size: %lld\n", sizeof(atoll(res)));
-    two_com(res);
-    printf("in binary: %s\n",res);
+
 
     return 0;
 }
