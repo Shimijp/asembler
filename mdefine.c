779e34c1a90915d2f714d21ca59cb41865f8e533
talaverbuch <tal3397@gmail.com>
2024-03-21 11:27:19 +0200

    in process

---
+++ mdefine.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "globals.h"
+#include "files.h"
+#include "memory.h"
+#include "mdefine.h"
+#include "macros.h" /*to use get_first_word*/
+
+FILE *open_am_file(char *name)
+{
+    FILE *nfp;
+    int new_len;
+    char *new_name;
+
+    new_len = (int)(strlen(name) + strlen(".am"));
+
+    init_str(&new_name,new_len);
+
+    strcpy(new_name,name);
+    strcat(new_name,".am");
+    printf("%s\n",name);
+    nfp = fopen(new_name, "w+");
+    if(!check_file(nfp))
+    {
+        perror("error!");
+        exit(EXIT_FAILURE);
+    }
+    free(new_name);
+    return nfp;
+}
+
+FILE *rewrite_signs(char *name)
+{
+    FILE *nfp;
+    sign *signs;
+    return 0;
+}
+
+bool v_name_exists(sign **ptr, char *name)
+{
+    sign *temp = *ptr;
+    while (temp != NULL)
+    {
+        if (strcmp(temp->v_name, name) == 0)
+        {
+            return true;
+        }
+    }
+    return false; /* name doesn't exist */
+}
+
+void add_first_sign(sign **ptr, char *name, int val)
+{
+    sign *new_sign = (sign *) malloc(sizeof(sign));
+
+    if (new_sign == NULL) {
+        perror("not enough memory");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        strncpy(new_sign->v_name, name, MAX_LABEL_LENGTH - 1);
+        new_sign->v_name[MAX_LABEL_LENGTH - 1] = '\0'; /* Ensure null termination */
+        new_sign->val = val;
+        new_sign->next = *ptr; /* Point to the old first node */
+        *ptr = new_sign; /* Update the head to the new node */
+    }
+}
+
+void add_last_sign(sign **ptr, char *name, int value)
+{
+    if (v_name_exists(ptr, name))
+    {
+        printf("Variable name '%s' already exists in mdefine table. Duplicate names are not allowed.\n", name);
+        exit(EXIT_FAILURE);
+    }
+    /* add check if label with the same name already exists */
+    if ((*ptr) == NULL)
+    {
+        add_first_sign(ptr, name, value);
+    }
+
+    else
+    {
+        sign *temp = *ptr;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = (sign *)malloc(sizeof(sign));
+        if (!check_malloc(temp->next))
+        {
+            exit(EXIT_FAILURE);
+        }
+        temp = temp->next;
+        strncpy(temp->v_name, name, MAX_LABEL_LENGTH -1);
+        temp->v_name[MAX_LABEL_LENGTH - 1] = '\0';
+        temp->val = value;
+        temp->next = NULL;
+    }
+}
+
+sign get_constants(FILE *nfp)
+{
+    sign *mdefine; /* the table that will hold hold all variables with int values in the file */
+
+    mdefine = NULL;
+
+    while
+
+}
---
+++ mdefine.h
@@ -0,0 +1,35 @@
+
+
+#ifndef ASEMBLER2_MDEFINE_H
+#define ASEMBLER2_MDEFINE_H
+
+#include "globals.h"
+#include "memory.h"
+#include "files.h"
+
+#define LABEL ":"
+#define MAX_LABEL_LENGTH 31 /*or 32 to include '\0'?*/
+
+#define DATA ".data"
+#define DEFINE ".define"
+#define STRING ".string"
+#define ENTRY ".entry"
+#define EXTERN ".extern"
+
+
+typedef struct sign {
+    char v_name[MAX_LABEL_LENGTH]; /*variable name*/
+    int val; /*variable values*/
+    struct sign *next; /*pointer to next sign in the nested list*/
+} sign;
+
+void add_first_sign(sign **ptr, char *name, int val);
+bool v_name_exists(sign **ptr, char *name);
+void add_last_sign(sign **ptr, char *name, int val);
+sign get_constants(FILE *nfp); /* adds the constants that are declared with .define */
+sign get_labels(FILE *nfp); /* adds the variables that are declared with a label and hold an int value */
+
+FILE *rewrite_signs(char *name);
+FILE *open_am_file(char *name);
+
+#endif //ASEMBLER2_MDEFINE_H
