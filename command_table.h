67dcb016ef696575951cef13c9370e2fb6bf1956
shimon daniel <shimiya18@gmail.com>
2024-03-26 11:33:41 +0200

    added functions

--- command_table.h
+++ command_table.h
@@ -10,28 +10,23 @@ typedef struct
     ARE digit:2;
     address s_address:2;
     address d_address:2;
-    optcode code;
+    optcode code:4;
     registers s_reg;
     registers d_reg;
     node * binary;
     char * instruc;
     int ic;
 
-
-
-
-
-
-
 }word;
-address get_s_add(char * line);
 ARE get_are(char * line);
-address get_d_add(char * line);
-optcode get_optcode(char * line);
+address get_s_address(char * line);
+address get_d_address(char * line);
 registers get_s_reg(char * line);
-registers get_d_reg(char * line);
-bool is_first_word(char * word);
-bool is_symbol(char * word);
+int get_amount_of_op(char * command);
+bool is_data(char * command);
+
+
+
 
 
 #endif //ASEMBLER2_COMMAND_TABLE_H
